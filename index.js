84cf19fc274a4edc99054df3eebc00995d2046ed
Adriel Borges <adrielluca@gmail.com>
2022-07-22 11:37:48 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const { parse, parseFile, formatters } = require('plantuml-parser');
+
+const data = `
+@startuml
+
+class Base
+class Refund
+class Repair
+
+Refund -> Base
+Repair -> Base
+
+@enduml
+`;
+
+// parse PlantUML
+const result = parse(data);
+
+// Format and print parse result
+console.log(
+  formatters.default(result)
+);
